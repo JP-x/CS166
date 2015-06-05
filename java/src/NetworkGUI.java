c5b4061b70b16f3b2b819f6986efe2715212286a
Tricia Hinkson <think001@well.cs.ucr.edu>
2015-06-05 00:04:40 -0700

    flow layout

--- java/src/NetworkGUI.java
+++ java/src/NetworkGUI.java
@@ -1,4 +1,4 @@
-//import package.ProfNetwork;
+//import ProfNetwork;
 import java.awt.EventQueue;
 import javax.swing.*;
 import javax.imageio.ImageIO;
@@ -12,17 +12,33 @@ import java.io.IOException;
 public class NetworkGUI extends JFrame{
     String title = "The Network";
     private BufferedImage image;
+    //ProfNetwork esql = new ProfNetwork();
     
     NetworkGUI(){
     setTitle(title);
     setSize(640,480);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
     FlowLayout experimentLayout = new FlowLayout();
+    
+    JLabel userLabel = new JLabel("Username: ");
+    JLabel passLabel = new JLabel("Password: ");
+    final JTextField userField = new JTextField(20);
+    final JTextField passField = new JTextField(20);
+    userLabel.setBounds(400,400,100,30);
+    passLabel.setBounds(400,500, 100, 30);
+    userField.setBounds(400, 400, 100, 30);
+    passField.setBounds(600, 600, 200, 30);
+    
     JButton b1 = new JButton("Create user");
     JButton b2 = new JButton("Log in");
     JButton b3 = new JButton("Exit");
     b1.setBounds(400,100,200,30);
     b2.setBounds(400,200,200,30);
+    b2.addActionListener(new java.awt.event.ActionListener(){
+      public void actionPerformed(java.awt.event.ActionEvent evt){
+        loginAction(evt, userField.getText(), passField.getText());
+      }
+    });
     b3.setBounds(400,300,200,30);
     b3.addActionListener(new java.awt.event.ActionListener(){
       public void actionPerformed(java.awt.event.ActionEvent evt){
@@ -36,14 +52,22 @@ public class NetworkGUI extends JFrame{
     add(label);
     add(b1);
     add(b2);
-    add(b3); 
-    pack();
+    add(b3);
+    add(userLabel);
+    add(userField);
+    add(passLabel);
+    add(passField); 
+    //pack();
     setVisible(true);
+  }
+  
+  private void loginAction(java.awt.event.ActionEvent evt, String user, String pass){
+  
   }
   
   private void exitAction(java.awt.event.ActionEvent evt){
     System.exit(0);
-  } 
+  }
     
     
     public static void main(String[] args){  
