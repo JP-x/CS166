d81578568b3569cfcfdb171dc60da8cc9e3324b5
Tricia Hinkson <think001@well.cs.ucr.edu>
2015-06-04 23:19:45 -0700

    exit button

--- java/src/NetworkGUI.java
+++ java/src/NetworkGUI.java
@@ -16,46 +16,37 @@ public class NetworkGUI extends JFrame{
     NetworkGUI(){
     setTitle(title);
     setSize(640,480);
-    //setLocationRelativeTo(null);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    //add("Center", esql);
     FlowLayout experimentLayout = new FlowLayout();
     JButton b1 = new JButton("Create user");
     JButton b2 = new JButton("Log in");
     JButton b3 = new JButton("Exit");
-    //setBounds(x,y,width,height
     b1.setBounds(400,100,200,30);
     b2.setBounds(400,200,200,30);
     b3.setBounds(400,300,200,30);
+    b3.addActionListener(new java.awt.event.ActionListener(){
+      public void actionPerformed(java.awt.event.ActionEvent evt){
+        exitAction(evt);
+      }
+    });
     
-        
     getContentPane().setLayout(new FlowLayout());
     ImageIcon icon = new ImageIcon("../images/networklogothing.png");
     JLabel label = new JLabel("", icon, JLabel.LEADING);
     add(label);
     add(b1);
     add(b2);
-    add(b3);
-    //JPasswordField passwordField = new JPasswordField(10);
-    //passwordField.setActionCommand(OK);
-    //passwordField.addActionListener(passwordField);
-    
-    //JPanel picpanel = new JPanel();
-    //ImageIcon logo = new ImageIcon("networklogothing.png");
-    //JLabel labeel = new JLabel(logo);
-    //add(labeel);
-    //picpanel.add(new JLabel(logo));
-    //add(picpanel);
-    
-    //ImageIcon logo = new ImageIcon("..\\images\\networklogothing.png");
-    
-    //setLayout(null);//no layout manager  
+    add(b3); 
     pack();
-    setVisible(true);//now frame will be visible, by default not visible  
-    }
+    setVisible(true);
+  }
+  
+  private void exitAction(java.awt.event.ActionEvent evt){
+    System.exit(0);
+  } 
     
     
     public static void main(String[] args){  
-        NetworkGUI main_menu=new NetworkGUI();  
+        NetworkGUI main_menu = new NetworkGUI();  
     }  
 }
