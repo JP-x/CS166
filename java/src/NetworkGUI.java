27cd614f9d486557f948c232b5c73767d9457fda
Jonathan Padilla <jpadi004@well.cs.ucr.edu>
2015-06-06 02:01:41 -0700

    diff login action + new screen

--- java/src/NetworkGUI.java
+++ java/src/NetworkGUI.java
@@ -3,6 +3,7 @@ package CS166;
 import CS166.*;
 import java.awt.EventQueue;
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import javax.swing.*;
 import javax.imageio.ImageIO;
 import java.awt.Graphics;
@@ -23,6 +24,9 @@ public class NetworkGUI extends JFrame{
     String title = "The Network";
     private BufferedImage image;
     ProfNetwork esql = null;
+    //private JPanel login_screen;
+    //private JPanel main_menu;
+    //private login_panel log;
     
     NetworkGUI(String[] args){
       try{
@@ -37,7 +41,7 @@ public class NetworkGUI extends JFrame{
       setTitle(title);
       setSize(640,480);
       setDefaultCloseOperation(EXIT_ON_CLOSE);
-      FlowLayout experimentLayout = new FlowLayout();
+      //FlowLayout experimentLayout = new FlowLayout();
       
       JLabel userLabel = new JLabel("Username: ");
       JLabel passLabel = new JLabel("Password: ");
@@ -51,55 +55,95 @@ public class NetworkGUI extends JFrame{
       JButton b1 = new JButton("Create user");
       JButton b2 = new JButton("Log in");
       JButton b3 = new JButton("Exit");
-      b1.setBounds(400,100,200,30);
-      b2.setBounds(400,200,200,30);
+      //b1.setBounds(400,100,200,30);
+      b1.setLocation(400,100);
+      b1.setSize(200,30);
+      //LOGIN BUTTON
+      //b2.setBounds(400,200,200,30);
+      b2.setLocation(400,200);
+      b2.setSize(200,30);
       b2.addActionListener(new java.awt.event.ActionListener(){
         public void actionPerformed(java.awt.event.ActionEvent evt){
-          loginAction(evt, userField.getText(), passField.getText());
+          int success = 0;
+          success = loginAction(evt, userField.getText(), passField.getText());
+          if(success == 1)
+          {
+              //clear out all components for new window
+              
+              //TODO: implement new panel to then add to the stupid frame
+              //EVERY BUTTON WILL HAVE ITS OWN ACTION PERFORMED
+              //WHICH WILL THEN CLEAR BASED ON SUCCESS VALUE
+              getContentPane().removeAll();
+              getContentPane().repaint();
+            JButton b4 = new JButton("Exit");
+            b4.setLocation(400,300);
+            b4.setSize(200,30);
+            b4.addActionListener(new java.awt.event.ActionListener(){
+            public void actionPerformed(java.awt.event.ActionEvent evt){
+            exitAction(evt);
+            }
+                });
+            
+             getContentPane().add(b4, BorderLayout.SOUTH);
+             setVisible(true);
+          }
         }
       });
-      b3.setBounds(400,300,200,30);
+      //EXIT BUTTON
+      //b3.setBounds(400,300,200,30);
+      b3.setLocation(400,300);
+      b3.setSize(200,30);
       b3.addActionListener(new java.awt.event.ActionListener(){
         public void actionPerformed(java.awt.event.ActionEvent evt){
           exitAction(evt);
         }
       });
       
+      
+     // JPanel main_menu = new JPanel();
+    //  main_menu.setLayout(new CardLayout());
+      
       ImageIcon icon = new ImageIcon("../images/networklogothing.png");
       JLabel label = new JLabel("", icon, JLabel.LEFT);
-      JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-      panel.add(label);
-      getContentPane().add(panel, BorderLayout.NORTH);
+      JPanel logo_panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+      logo_panel.add(label);
+      getContentPane().add(logo_panel, BorderLayout.NORTH);
       
-      panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-      panel.add(userLabel);
-      panel.add(userField);
-      panel.add(passLabel);
-      panel.add(passField);
-      getContentPane().add(panel, BorderLayout.CENTER); 
+      JPanel all_login = new JPanel(new CardLayout());
       
-      panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-      panel.add(b1);
-      panel.add(b2);
-      panel.add(b3);
-      getContentPane().add(panel, BorderLayout.SOUTH);
+      JPanel loginpanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+      loginpanel.add(userLabel);
+      loginpanel.add(userField);
+      loginpanel.add(passLabel);
+      loginpanel.add(passField);
+      getContentPane().add(loginpanel, BorderLayout.CENTER); 
+      
+      JPanel buttons_panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+      buttons_panel.add(b2);
+      buttons_panel.add(b1);
+      buttons_panel.add(b3);
+      //main_menu.add(panel, BorderLayout.NORTH);
+      getContentPane().add(buttons_panel, BorderLayout.SOUTH);
+      
+      //getContentPane().add(main_menu,FlowLayout.CENTER);
       
       pack();
       setVisible(true);
   }
   
-  private void loginAction(java.awt.event.ActionEvent evt, String user, String pass){
+  private int loginAction(java.awt.event.ActionEvent evt, String user, String pass){
     String status = ProfNetwork.LogIn(esql, user, pass);
     System.out.println("Username: " + user);
     System.out.println("Password: " + pass);
     if(status.equals("login")){
       //Login Passed
       System.out.println("Go to User Profile.");
-      
+      return 1;
     }
     else{
       //Login Failed
       System.out.println("Login Failed");
+      return -1;
     }
   }
   
@@ -112,3 +156,36 @@ public class NetworkGUI extends JFrame{
         NetworkGUI main_menu = new NetworkGUI(args);  
     }  
 }
+
+/*
+class login_panel extends JPanel{
+    private JButton jcomp1;
+    private JButton jcomp2;
+    private JButton jcomp3;
+    private JTextField jcomp4;
+
+    public login_panel() {
+        //construct components
+        jcomp1 = new JButton ("test1");
+        jcomp2 = new JButton ("test2");
+        jcomp3 = new JButton ("test3");
+        jcomp4 = new JTextField (5);
+
+        //adjust size and set layout
+        setSize (640, 480);
+        setLayout (null);
+
+        //set component bounds (only needed by Absolute Positioning)
+        jcomp1.setBounds (20, 45, 100, 25);
+        jcomp2.setBounds (135, 60, 100, 25);
+        jcomp3.setBounds (260, 35, 100, 25);
+        jcomp4.setBounds (105, 115, 100, 25);
+
+        //add components
+        add (jcomp1);
+        add (jcomp2);
+        add (jcomp3);
+        add (jcomp4);       
+    }
+}
+*/
